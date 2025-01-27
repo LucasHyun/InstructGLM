751605cadcd8312658a5d7fe65f35c06813e8894
Hlucomeister <ehyun99@gmail.com>
2025-01-27 03:04:06 -0500

    sdfsdf

--- llama_pubmed_src/PubMed.py
+++ llama_pubmed_src/PubMed.py
@@ -1211,14 +1211,15 @@ class PubMed_Dataset(Dataset):
                                         go_on_L2 = True
                                         
                                         while go_on_L2:
-                                            if len([hop for hop in train_L2_undepleted if hop[0] == select_L1]) > 0:
+                                            toselectfrom = [hop for hop in train_L2_undepleted if hop[0] == select_L1]
+                                            if len(toselectfrom) > 0:
                                                 # select random node from select_L1_hops
-                                                weights_L2 = [simdeg_L2_dict[pair[1]] for pair in select_L1_hops]
+                                                weights_L2 = [simdeg_L2_dict[pair[1]] for pair in toselectfrom]
                                                 select_L2_ID = np.random.choice(
-                                                    range(len(select_L1_hops)),
+                                                    range(len(toselectfrom)),
                                                     p = weights_L2 / np.sum(weights_L2)
                                                 )
-                                                select_L2 = select_L1_hops[select_L2_ID]
+                                                select_L2 = toselectfrom[select_L2_ID]
                                                 
                                                 # check number of L3 hops not in already_idx
                                                 select_L2_hops = [hop for hop in train_L3 if hop[0] == select_L1 and hop[1] == select_L2[1] and hop not in already_idx]
@@ -4932,7 +4933,11 @@ class PubMed_Dataset(Dataset):
                                             if len(select_L2_hops) > 0:
                                                 go_on_L2 = False
                                             else:
-                                                train_L2_undepleted.remove(select_L2)
+                                                try:
+                                                    train_L2_undepleted.remove(select_L2)
+                                                except:
+                                                    print(train_L2_undepleted)
+                                                    print(select_L2)
                                                 select_L2 = None
                                         else:
                                             go_on_L2 = False
